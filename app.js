f1c131247cc0e33e27fee4c342c770df50111676
Maxx730 <max.kinghorn@gmail.com>
2021-06-25 11:23:55 -0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const port = process.env.PORT || 4000;
+const { debug } = require('console');
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://videosync-client-kw3it.ondigitalocean.app/',
+    origin: '*',
     methods: ["GET", "POST"]
   }
 });
@@ -14,20 +15,26 @@ const users = new Array();
 const videos = new Array();
 
 io.on('connection', (socket) => {
-    if (!users.filter(user => user.id === socket.id).length) {
-      users.push({
-        id: socket.id,
-        nickname: 'Client-' + socket.id
-      });
-    }  
-
-    socket.on('disconnect', socket => {
+    socket.on('disconnect', () => {
       users.forEach(user => {
-        if (user.id === socket.id) {
-          console.log('removing disconnected player');
+        if (user.nickname === socket.data.nickname) {
+          const index = users.indexOf(user);
+          users.splice(index, 1);
+          io.emit('users_updated', users);
         } 
       });
     });
+
+    socket.on('user_login', nickname => {
+      if (!users.filter(user => user.nickname === nickname).length) {
+        console.log(nickname + ' Connected!');
+        users.push({
+          nickname: nickname
+        });
+        socket.data = {nickname: nickname};
+        io.emit('users_updated', users);
+      }  
+    })
 });
 
 server.listen(port, () => {

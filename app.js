c40b1b41a0dd855a646ff4bfa1d8081ee61893ce
Maxx730 <max.kinghorn@gmail.com>
2021-06-25 06:14:33 -0700

    testing connections

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+const port = process.env.PORT || 4000;
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const http = require('http');
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://videosync-client.herokuapp.com/',
+    methods: ["GET", "POST"]
+  }
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const users = new Array();
+const videos = new Array();
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+io.on('connection', (socket) => {
+    if (!users.filter(user => user.id === socket.id).length) {
+      users.push({
+        id: socket.id,
+        nickname: 'Client-' + socket.id
+      });
+    }  
+
+    socket.on('disconnect', socket => {
+      users.forEach(user => {
+        if (user.id === socket.id) {
+          console.log('removing disconnected player');
+        } 
+      });
+    });
+});
+
+server.listen(port, () => {
+  console.log(`Video Sync Core listening on port:` + port);
+});
